75e3ccf36c8de05f67c4686ff33ee6742549a486
tom <JosephDotson@belkonar.com>
2026-03-07 18:27:03 -0500

    tap

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,30 @@
 #include <QtWidgets/qlabel.h>
+#include <QtWidgets/qmainwindow.h>
 #include <QtWidgets/qwidget.h>
 #include <QtWidgets>
+#include <memory>
 
-int main(int argc, char *argv[]) {
-    QApplication a(argc, argv);
+std::shared_ptr<QMainWindow> render(const char *text) {
 
-    QMainWindow window;
+    auto window = std::make_shared<QMainWindow>();
 
     QWidget *container = new QWidget();
     QLabel *label = new QLabel(container);
 
-    label->setText("Hi.");
+    label->setText(text);
 
-    window.setCentralWidget(container);
+    window->setCentralWidget(container);
 
-    window.show();
+    window->show();
+
+    return window;
+}
+
+int main(int argc, char *argv[]) {
+    QApplication a(argc, argv);
+
+    auto w1 = render("Hi");
+    auto w2 = render("Dave");
 
     return a.exec();
 }
